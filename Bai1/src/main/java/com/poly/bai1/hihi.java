5b09ea31d3bf000fb01bccedc1c4562fe57dc67f
linhtd15 <linhtd1212@gmail.com>
2020-01-08 21:47:31 +0700

    Linh Sửa

--- Bai1/src/main/java/com/poly/bai1/hihi.java
+++ Bai1/src/main/java/com/poly/bai1/hihi.java
@@ -9,8 +9,6 @@ package com.poly.bai1;
  *
  * @author LinhTD
  */
-public class HelloWorld {
-    public static void main(String[] args) {
-        System.out.println("Hello World");
-    }
+public class hihi {
+    
 }
