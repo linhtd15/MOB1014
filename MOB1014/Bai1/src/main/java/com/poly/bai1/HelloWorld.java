93e51fec9b1c4bc31880efb0466767cd5a123dce
linhtd15 <linhtd1212@gmail.com>
2020-01-10 23:15:06 +0700

    ok

---
+++ MOB1014/Bai1/src/main/java/com/poly/bai1/HelloWorld.java
@@ -0,0 +1,11 @@
+package com.poly.bai1;
+
+/**
+ *
+ * @author LinhTD
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
---
+++ MOB1014/Bai1/src/main/java/com/poly/bai1/NextMethod.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.poly.bai1;
+import java.util.Scanner;
+/**
+ *
+ * @author LinhTD
+ */
+public class NextMethod {
+    public static void main(String[] args) {
+//        Scanner sc = new Scanner(System.in); //Tạo đối tượng Scanner
+//        System.out.print("Nhap du lieu: ");
+//        String a = sc.nextLine(); // nhận vào 1 string
+//        System.out.println("Du lieu vua nhap: "+a);
+//
+//        System.out.print("Nhap tiep du lieu: ");
+//        String b = sc.next(); // cũng nhận vào 1 String
+//        System.out.println("Du lieu vua nhap: "+b);
+        
+        int a,b,c;
+        Scanner sc = new Scanner(System.in); // Tạo đối tượng Scanner
+        System.out.print("Nhap a: ");
+        a = sc.nextInt();
+        b = sc.nextInt();
+        c = sc.nextInt();
+        System.out.println("a = "+a);
+        System.out.println("b = "+b);
+        System.out.println("c = "+c);
+    }
+}
