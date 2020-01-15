56300a89bbc9cf90fa7335d3d0a0734e1d49ae40
linhtd15 <linhtd1212@gmail.com>
2020-01-15 10:04:08 +0700

    ok

--- Bai1/src/main/java/com/poly/bai1/InputData.java
+++ Bai1/src/main/java/com/poly/bai1/InputData.java
@@ -1,17 +1,12 @@
 package com.poly.bai1;
 
 import java.util.Scanner;
-
-/**
- *
- * @author LinhTD
- */
 public class InputData {
     public static void main(String[] args) {
         Scanner scanner=new Scanner(System.in);
         int a = scanner.nextInt();
         float b = scanner.nextFloat();
         System.out.println("Gia tri cua a = " + a);
-        System.out.println("Gia tri cua a = " + b);
+        System.out.println("Gia tri cua b = " + b);
     }
 }
--- Bai1/src/main/java/com/poly/bai1/NextMethod.java
+++ Bai1/src/main/java/com/poly/bai1/NextMethod.java
@@ -11,23 +11,23 @@ import java.util.Scanner;
  */
 public class NextMethod {
     public static void main(String[] args) {
-//        Scanner sc = new Scanner(System.in); //Tạo đối tượng Scanner
-//        System.out.print("Nhap du lieu: ");
-//        String a = sc.nextLine(); // nhận vào 1 string
-//        System.out.println("Du lieu vua nhap: "+a);
-//
-//        System.out.print("Nhap tiep du lieu: ");
-//        String b = sc.next(); // cũng nhận vào 1 String
-//        System.out.println("Du lieu vua nhap: "+b);
+        Scanner sc = new Scanner(System.in); //Tạo đối tượng Scanner
+        System.out.print("Nhap du lieu: ");
+        String a = sc.nextLine(); // nhận vào 1 string
+        System.out.println("Du lieu vua nhap: "+a);
+
+        System.out.print("Nhap tiep du lieu: ");
+        String b = sc.next(); // cũng nhận vào 1 String
+        System.out.println("Du lieu vua nhap: "+b);
         
-        int a,b,c;
-        Scanner sc = new Scanner(System.in); // Tạo đối tượng Scanner
-        System.out.print("Nhap a: ");
-        a = sc.nextInt();
-        b = sc.nextInt();
-        c = sc.nextInt();
-        System.out.println("a = "+a);
-        System.out.println("b = "+b);
-        System.out.println("c = "+c);
+//        int a,b,c;
+//        Scanner sc = new Scanner(System.in); // Tạo đối tượng Scanner
+//        System.out.print("Nhap a: ");
+//        a = sc.nextInt();
+//        b = sc.nextInt();
+//        c = sc.nextInt();
+//        System.out.println("a = "+a);
+//        System.out.println("b = "+b);
+//        System.out.println("c = "+c);
     }
 }
