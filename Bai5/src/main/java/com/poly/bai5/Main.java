cdbe5955efa450f9aedf216cf3c75b192699b8ee
linhtd15 <linhtd1212@gmail.com>
2020-02-20 16:14:20 +0700

    ok

---
+++ Bai5/src/main/java/com/poly/bai5/Main.java
@@ -0,0 +1,49 @@
+package com.poly.bai5;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void menu(){
+        System.out.println("======== CHUONG TRINH QUAN LY SINH VIEN ========");
+        System.out.println("1. Them sinh vien");
+        System.out.println("2. Hien thi danh sach sinh vien");
+        System.out.println("3. Sua sinh vien theo ma");
+        System.out.println("4. Xoa sinh vien theo ma");
+        System.out.println("0. Thoat chuong trinh");
+        System.out.println("================================================");
+        System.out.print("Chon chuc nang [0-4]: ");
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        QuanLySinhVien qlSV = new QuanLySinhVien();
+        menu();
+        int fn;
+        boolean thoat = false;
+        while(true){
+            fn = sc.nextInt();
+            switch(fn){
+                case 1:
+                    qlSV.themSinhVien();
+                    break;
+                case 2:
+                    qlSV.hienThiSinhVien();
+                    break;
+                case 3:
+                    System.out.print("Nhap ma sinh vien can sua: ");
+                    qlSV.suaSinhVien(new Scanner(System.in).nextLine());
+                    break;
+                case 4:
+                    System.out.print("Nhap ma sinh vien can xoa: ");
+                    qlSV.xoaSinhVien(new Scanner(System.in).nextLine());
+                    System.out.print("Da xoa sinh vien thanh cong! ");
+                    break;
+                case 0:
+                    System.out.println("Thoat chuong trinh");
+                    thoat = true;
+                    break;
+            }
+            if(thoat) break;
+            menu();
+        }
+    }
+}
--- Bai5/src/main/java/com/poly/bai5/QuanLySinhVien.java
+++ Bai5/src/main/java/com/poly/bai5/QuanLySinhVien.java
@@ -8,16 +8,18 @@ public class QuanLySinhVien {
     Scanner sc = new Scanner(System.in);
     private ArrayList<SinhVien> dsSinhVien;
 
+    //Phương thức khởi tạo
+    public QuanLySinhVien() {
+        dsSinhVien = new ArrayList<SinhVien>();
+    }
+
+    //Getter và Getter
     public void setDsSinhVien(ArrayList<SinhVien> dsSinhVien) {
         this.dsSinhVien = dsSinhVien;
     }
 
     public ArrayList<SinhVien> getDsSinhVien() {
         return dsSinhVien;
-    }
-
-    public QuanLySinhVien() {
-        dsSinhVien = new ArrayList<SinhVien>();
     }
 
     //Kiểm tra tính hợp lệ và nhập điểm
@@ -33,5 +35,71 @@ public class QuanLySinhVien {
                 System.out.print("Nhap sai diem, hay nhap lai: ");
             }
         }
+    }
+
+    //Thêm sinh viên
+    public void themSinhVien() {
+        System.out.println(">>> Nhap thong tin sinh vien: ");
+        System.out.print("\tMa sinh vien: ");
+        String maSV = sc.nextLine();
+        System.out.print("\tTen sinh vien: ");
+        String tenSV = sc.nextLine();
+        System.out.print("\tDiem Java1: ");
+        float diemJava1 = nhapDiemJava();
+        System.out.print("\tDiem Java2: ");
+        float diemJava2 = nhapDiemJava();
+        SinhVien sv = new SinhVien(maSV, tenSV, diemJava1, diemJava2);
+        dsSinhVien.add(sv);
+    }
+
+    //Hiển thị danh sách sinh viên
+    public void hienThiSinhVien() {
+        System.out.println(">>>>> DANH SACH SINH VIEN <<<<<");
+        System.out.format("%-10s ", "Ma SV");
+        System.out.format("%-20s ", "Ten Sinh vien");
+        System.out.format("%-10s ", "Diem Java1");
+        System.out.format("%-10s ", "Diem Java2");
+        System.out.format("%-10s ", "Diem TB");
+        System.out.format("%-15s\n", "Xep loai");
+        //Duyệt và hiển thị danh sách sinh viên
+        for (SinhVien sv : dsSinhVien) {
+            System.out.format("%-10s ", sv.getMaSV());
+            System.out.format("%-20s ", sv.getTenSV());
+            System.out.format("%-10.1f ", sv.getDiemJava1());
+            System.out.format("%-10.1f ", sv.getDiemJava2());
+            System.out.format("%-10.1f ", sv.diemTrungBinh());
+            System.out.format("%-15s\n", sv.xepLoai());
+        }
+    }
+    //Sửa sinh viên theo mã sinh viên
+    public void suaSinhVien(String maSV){
+        boolean tonTai = false;
+        for(int i=0; i<dsSinhVien.size();i++){
+            if(dsSinhVien.get(i).getMaSV().equalsIgnoreCase(maSV)){
+                tonTai=true;
+                System.out.print("Nhap ten sinh vien: ");
+                dsSinhVien.get(i).setTenSV(sc.nextLine());
+                System.out.print("Nhap diem Java1: ");
+                dsSinhVien.get(i).setDiemJava1(nhapDiemJava());
+                System.out.print("Nhap diem Java2: ");
+                dsSinhVien.get(i).setDiemJava2(nhapDiemJava());
+                break;
+            }  
+        }
+        if(!tonTai) System.out.println("Khong tim thay sinh vien co ma "+maSV);
+    }
+    //Xóa sinh viên
+    public void xoaSinhVien(String maSV){
+        SinhVien sv=null;
+        for(int i=0;i<dsSinhVien.size();i++){
+            if(dsSinhVien.get(i).getMaSV().equalsIgnoreCase(maSV)){
+                sv = dsSinhVien.get(i);
+                break;
+            }
+        }
+        if(sv != null){
+            dsSinhVien.remove(sv);
+        }
+        else System.out.println("Khong tim thay sinh vien co ma "+maSV);
     }
 }
